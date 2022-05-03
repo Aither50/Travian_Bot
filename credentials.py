5f40c655665b803c0c3d2ac2f408348df53fc618
Mathieu <37325098+Aither50@users.noreply.github.com>
2022-05-03 19:50:58 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,17 +1,21 @@
-from configparser import ConfigParser
+import configparser
 
-config = ConfigParser()
-config.sections()
+config = configparser.ConfigParser()
 config.read('config.ini')
 
-SERVER_URL = config['USER']['server']
-VILLAGE_URL = SERVER_URL + 'dorf1.php'
-USERNAME = config['USER']['username']
+SERVER_URL = config['USER']['server_url']
+USERNAME = config['USER']['email']
 PASSWORD = config['USER']['password']
+PROXY = {'http': config['USER']['https_proxy'],
+         'https': config['USER']['https_proxy']}
 
 HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'en,en-US;q=0.8,ru-RU;q=0.6,ru;q=0.4',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
     'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
 }
+
+VILLAGE_URL = SERVER_URL + 'dorf1.php'
+TOWN_URL = SERVER_URL + 'dorf2.php'
+HERO_URL = SERVER_URL + 'hero.php?t=3'
